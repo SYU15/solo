7609be5a712d4f11a167a206f372779138ff17a4
Sarah Yu <sarah.m.yu88@gmail.com>
2015-06-15 16:32:04 -0700

    D3 animations/bar graph for stats now appearing, simple server now serving index.html

--- client/dumplingView.js
+++ client/dumplingView.js
@@ -1,7 +1,7 @@
 var DumplingView = Backbone.View.extend({
   /* Fill out your solution here */
 
-  template: _.template('<h2 class="dumView">Home</h2><h2 class="statsView">Stats</h2><h2 class="name"><%= name %>, says hi!</h2><div class="dum"><img class="dumImg" src="<%= image %>"/><div>'),
+  template: _.template('<h2 class="dumView">Home</h2><h2 class="statsView">Stats</h2><h2 class="name"><%= name %>, says hi!</h2><img class="dumImg" src="<%= image %>"/>'),
 
   events: {
     'click .statsView': 'changeToStats'
@@ -18,6 +18,17 @@ var DumplingView = Backbone.View.extend({
     this.$el.show();
     this.$el.children().detach();
     this.collection.forEach(this.renderDumpling, this);
+    // console.log(this.$el.children('img'));
+    var imgEl = this.$el.children('img')[0];
+    var move = function(){d3.select(imgEl)
+        .style({'margin-left': '-300px'})
+        .transition().duration(2000)
+        .style({'margin-left':'300px'})
+        .transition().duration(2000)
+        .style({'margin-left':'-300px'});
+      };
+      move();    
+      setInterval(move, 4000); 
   },
 
   renderDumpling: function(item) {
--- client/statsView.js
+++ client/statsView.js
@@ -1,7 +1,7 @@
 var StatsView = Backbone.View.extend({
   /* Fill out your solution here */
 
-  template: _.template('<h2 class="dumView">Home</h2><h2 class="statsView">Stats</h2><div class="stats"><%= name %><%= age %><%= health %><div>'),
+  template: _.template('<h2 class="dumView">Home</h2><h2 class="statsView">Stats</h2><div class="stats"><h2>Name: <%= name %></h2><h2>Type: <%= type %></h2></div>'),
 
   events: {
     'click .dumView': 'changeToDums'
@@ -16,11 +16,53 @@ var StatsView = Backbone.View.extend({
   render: function() {
     this.$el.show();
     this.$el.children().detach();
-    this.collection.forEach(this.renderDumpling, this);
+    this.collection.forEach(this.renderStats, this);
   },
 
-  renderDumpling: function(item) {
+  renderStats: function(item) {
     this.$el.append(this.template(item.attributes));
+    var bardata = [item.attributes.health, item.attributes.age,
+    item.attributes.juiciness, item.attributes.intelligence];
+    var barvalue = ['health', 'age', 'juiciness', 'intelligence'];
+    var width = 300;
+    // var height = 400;
+    var barWidth = 50;
+    var barOffset = 5;
+    var colors = d3.scale.linear()
+      .domain([0, d3.max(bardata)])
+      .range(['#ffb832', '#c61c6f']);
+    var height;
+    d3.select(this.el)
+      .append('svg')
+      .selectAll('rect')
+      .data(bardata)
+      .enter().append('rect')
+      .style('fill', colors)
+      .attr('width', barWidth)
+      .attr('height', function(d,i){
+        height = d * 10;
+        return height;
+      })
+      .attr('x', function(d, i){
+        return i * (barWidth + barOffset);
+      })
+      .attr('y', function(d){
+        return height - d;
+      });
+    d3.select('svg')
+    // .append('svg')
+    .selectAll('text')
+    .data(barvalue)
+    .enter().append('text')
+    .attr('font-size', '13px')
+    .attr('width', 100)
+    .attr('x', function(d, i){
+        return i * 60;
+      })
+    .attr('y', 80)
+    .text(function(d){
+      return d;
+    });
   },
   changeToDums: function() {
     this.$el.hide();
