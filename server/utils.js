b94513f060f2f7d79559ca4a3df67750ca56476a
Sarah Yu <sarah.m.yu88@gmail.com>
2015-07-05 08:48:40 -0700

    Added error handling

--- server/utils.js
+++ server/utils.js
@@ -3,6 +3,10 @@ var User = request('app.js');
 
 exports.getData = function(res, req){
   User.find({}).exec(function(err,user) {
+    console.log('running');
+    if(err){
+      console.log(err);
+    }
     res.send(200, user);
   })
 };
